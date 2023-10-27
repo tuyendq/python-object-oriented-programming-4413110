0c160bad05a99ed4173e849630ba480258bc1c5d
DANG QUOC TUYEN <tuyendq@gmail.com>
2023-10-27 15:46:51 +0000

    check instance

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,14 @@
 
 
 # TODO: create a basic class
-
+class Book:
+  def __init__(self, title):
+    self.title = title
 
 # TODO: create instances of the class
-
+book1 = Book("The Game of Life")
+book2 = Book("The Art of War")
 
 # TODO: print the class and property
+print(book1)
+print(book2.title)
--- Start/Ch 1/instance_start.py
+++ Start/Ch 1/instance_start.py
@@ -5,21 +5,34 @@
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title):
+    def __init__(self, title, price):
         self.title = title
         # TODO: add properties
-
+        self.price = price
+        self.__secret = "This is a secret attribute"
     # TODO: create instance methods
-
+    def getprice(self):
+        if (hasattr(self, "_discount")):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
+    
+    def setdiscount(self, amount):
+        self._discount = amount
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace", 9.99)
+b2 = Book("The Catcher in the Rye", 22.05)
 
 # TODO: print the price of book1
-
+print(b1.price)
+print(b2.price)
+print(b2.getprice())
 
 # TODO: try setting the discount
-
+b2.setdiscount(0.25)
+print(b2.getprice())
 
 # TODO: properties with double underscores are hidden by the interpreter
+# print(b1.__secret)
+print(b2._Book__secret)
